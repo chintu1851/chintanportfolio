3d32f0767751807fbb307183f2021fcffee16a58
chintu1851 <115821667+chintu1851@users.noreply.github.com>
2024-01-05 04:07:44 -0500

    second commit

--- src/App.js
+++ src/App.js
@@ -7,20 +7,23 @@ import Skills from './Skills';
 import About from './About';
 import Contact from './Contact';
 import Education from './Education'
-
+import { BrowserRouter } from 'react-router-dom';
 const App = () => {
   return (
-    <Router>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
       <Routes>
-        <Route path="/" element = {<Home></Home>}></Route>
-        <Route path="/projects" element = {<Projects></Projects>}></Route>
-        <Route path="/skills" element = {<Skills></Skills>}></Route>
-        <Route path="/about" element = {<About></About>}></Route>
-        <Route path="/contact" element = {<Contact></Contact>}></Route>
-        <Route path="/education" element = {<Education></Education>}></Route>
+        <Route path="/" element={<Home></Home>}></Route>
+        <Route path="/projects" element={<Projects></Projects>}></Route>
+        <Route path="/skills" element={<Skills></Skills>}></Route>
+        <Route path="/about" element={<About></About>}></Route>
+        <Route path="/contact" element={<Contact></Contact>}></Route>
+        <Route path="/education" element={<Education></Education>}></Route>
       </Routes>
-    </Router>
-    
+
+
+    </BrowserRouter>
+
+
   )
 }
 
