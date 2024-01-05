abb9ac26e846092ff2b17ce15981a7e7ef7ce767
chintu1851 <115821667+chintu1851@users.noreply.github.com>
2024-01-05 02:05:20 -0500

    second commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
+import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
- <div>
-  <p>Chintan Patel</p>
- </div>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+      <p>Chintan Patel</p>
+    </div>
   );
 }
 
